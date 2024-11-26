f788a8510db73cac322013d235b0e73767502955
Jose56-Visual <150677202+Jose56-Visual@users.noreply.github.com>
2024-11-26 10:37:29 +0100

    Add files via upload

---
+++ www/UD3/entregaTarea/menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky">
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="usuarios.php">Lista de Usuarios</a></li>
+            <li><a href="tareas.php">Lista de Tareas</a></li>
+            <li><a href="init.php">Inicializar Aplicación</a></li>
+        </ul>
+    </div>
+</nav> 
---
+++ www/UD3/entregaTarea/nuevoUsuarioForm.php
@@ -0,0 +1,12 @@
+<!-- nuevoUsuarioForm.php -->
+<form action="nuevoUsuario.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required><br>
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre" required><br>
+    <label for="apellidos">Apellidos:</label>
+    <input type="text" id="apellidos" name="apellidos" required><br>
+    <label for="contrasena">Contraseña:</label>
+    <input type="password" id="contrasena" name="contrasena" required><br>
+    <input type="submit" value="Crear Usuario">
+</form>
